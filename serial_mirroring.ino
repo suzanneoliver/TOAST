f296f67c31efa110b99e79af2cacf03a72554ec1
suzanneoliver <suzanneoliverreally@hotmail.com>
2018-01-15 21:04:42 -0800

    Add files via upload

---
+++ serial_mirroring.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(13,12); //RX,T
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+  while(!Serial)
+  { 
+    ;
+  }
+
+  Serial.println("Test 1");
+
+  mySerial.begin(19200);
+  mySerial.println("Test 2");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (mySerial.available())
+  {
+    Serial.write(mySerial.read());
+  }
+
+  if (Serial.available())
+  {
+    mySerial.write(Serial.read());
+  }
+}
